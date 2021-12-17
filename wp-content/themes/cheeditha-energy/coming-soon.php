123cf95d8b783ed594f0dd7ec175ebe54521693d
Jill Augustus <Jma529@gmail.com>
2021-12-17 17:28:03 +0800

    Change front page to landing page

--- wp-content/themes/cheeditha-energy/coming-soon.php
+++ wp-content/themes/cheeditha-energy/coming-soon.php
@@ -8,26 +8,35 @@
  */
 /* Template Name: Coming Soon */ 
 
-get_header(); ?>
+@include 'header-light.php' ?>
 
 <main class="coming-soon">
 <div class="wrapper">
  
   <div class="flex">
-      <div class="title">
-        <h1>New site coming soon...</h1>
-      </div>
+     
       <div class="site-logo">
-      <a href="<?php echo get_home_url(); ?>" class="header-logo">
         <img class="logo" src="<?php echo get_template_directory_uri(); ?>/media/images/logo-cheeditha.svg" alt="<?php bloginfo( 'name' ); ?>" />
-      </a>
     </div>
     <div class="animation-wrap coming-soon">
       <video playsinline autoplay muted loop>
         <source src="<?php echo get_template_directory_uri(); ?>/media/cheeditha-animation.mp4" type="video/mp4">
       </video>
     </div>
+    <div class="contact-info">
+        <h1>New site coming soon...</h1>
+        <h2>Need to get in touch?</h2>
+				<p>
+					<a href="mailto:enquire@cheeditha.com.au">enquire@cheedithaenergy.com.au</a>
+				</p>
+				<p>
+					<a href="tel:1300861537">1300861537</a>
+				</p>
+			</div>
+
 </div>
 </div>
+
+
 
 <?php get_footer(); ?>
--- wp-content/themes/cheeditha-energy/front-page.php
+++ wp-content/themes/cheeditha-energy/front-page.php
@@ -8,26 +8,33 @@
  */
 /* Template Name: Home */ 
 
-get_header(); ?>
+@include 'header-light.php' ?>
 
-<main class="front-page">
+<main class="coming-soon">
+<div class="wrapper">
+ 
+  <div class="flex">
+     
+      <div class="site-logo">
+        <img class="logo" src="<?php echo get_template_directory_uri(); ?>/media/images/logo-cheeditha.svg" alt="<?php bloginfo( 'name' ); ?>" />
+    </div>
+    <div class="animation-wrap coming-soon">
+      <video playsinline autoplay muted loop>
+        <source src="<?php echo get_template_directory_uri(); ?>/media/cheeditha-animation.mp4" type="video/mp4">
+      </video>
+    </div>
+    <div class="contact-info">
+        <h1>New site coming soon...</h1>
+        <h2>Need to get in touch?</h2>
+				<p>
+					<a href="mailto:enquire@cheeditha.com.au">enquire@cheedithaenergy.com.au</a>
+				</p>
+				<p>
+					<a href="tel:1300861537">1300861537</a>
+				</p>
+			</div>
 
-<?php 
-
-// get_template_part('template-parts/front-page/hero');
-
-get_template_part('template-parts/front-page/about');
-
-get_template_part('template-parts/front-page/team');
-
-get_template_part('template-parts/front-page/portfolio');
-
-get_template_part('template-parts/front-page/partners');
-
-get_template_part('template-parts/front-page/contact');
-
-?>
-
-</main>
+</div>
+</div>
 
 <?php get_footer(); ?>
