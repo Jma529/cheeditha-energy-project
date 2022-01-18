c8a170cf05d90c92f44916377109d5ad77fd074a
Jill Augustus <Jma529@gmail.com>
2022-01-18 15:15:50 +0800

    Add partner grid

--- wp-content/themes/cheeditha-energy/template-parts/front-page/partners.php
+++ wp-content/themes/cheeditha-energy/template-parts/front-page/partners.php
@@ -20,21 +20,24 @@ $title = get_field('partner_title');
     <div class="close">Close</div>
     <div class="content flex partner-list">
       <h1><?php echo $title?></h1>
-
+    <div class="partner-grid">
       <?php while( $partners -> have_posts() ) : $partners -> the_post(); 
       
         $image = get_the_post_thumbnail_url( get_the_ID(), 'full' );
         $link = get_field('partner_link');
+        $description = get_the_content();
         ?>
   
-    <div class="partner">
-      <a href="<?php echo $link; ?>"> 
-        <img class="partner-logo" src="<?php echo $image ?>');"/>
-      </a>
-    </div>
+        <div class="partner">
+            <a href="<?php echo $link; ?>"> 
+              <img class="partner-logo" src="<?php echo $image ?>');"/>
+            </a>
+            <p><?php echo $description ?></p>
+        </div>
+       
 
       <?php endwhile; wp_reset_postdata(); ?>
-      
+      </div> 
     </div>
 
   </div>
