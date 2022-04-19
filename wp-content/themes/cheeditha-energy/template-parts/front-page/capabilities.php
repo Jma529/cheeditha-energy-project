def2916b647c0fb9a463bbbeb68cd92d0d947d1b
Jill Augustus <Jma529@gmail.com>
2022-04-19 20:16:00 +0800

    fix post type name

--- wp-content/themes/cheeditha-energy/template-parts/front-page/capabilities.php
+++ wp-content/themes/cheeditha-energy/template-parts/front-page/capabilities.php
@@ -2,10 +2,11 @@
 
 /* Variables */
 $title = get_field('page_title');
+$summary = get_field('page_summary');
 
 /* Post query */
   $args = array(
-    'post_type'         => 'capability',
+    'post_type'         => 'capabilities',
     'orderby'           => 'menu_order',
     'order'             => 'ASC',
     'posts_per_page'    => -1
@@ -19,18 +20,18 @@ $title = get_field('page_title');
   <div class="wrapper">
     <div class="close">Close</div>
     <div class="content flex">
-      <h1><?php echo $title?></h1>
-      <p><?php the_content(); ?></p>
+      <h1><?php echo $title ?></h1>
+      <p><?php echo $summary ?></p>
       <div class="partner-grid">
       <?php while( $capabilities -> have_posts() ) : $capabilities -> the_post(); 
       
         $image = get_the_post_thumbnail_url( get_the_ID(), 'full' );
         $link = get_field('capabilities_link');
-        $description = get_field('page_summary');
+        $description = get_the_content();
         ?>
   
         <div class="partner">
-            <a href="<?php echo $link; ?>" target="_blank"> 
+            <a href="<?php echo $link ?>" target="_blank"> 
               <img class="partner-logo" src="<?php echo $image ?>');"/>
             </a>
             <p><?php echo $description ?></p>
@@ -40,6 +41,7 @@ $title = get_field('page_title');
       <?php endwhile; wp_reset_postdata(); ?>
 
       </div> 
+
     </div>
   </div>
 </section>
